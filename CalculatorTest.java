7a5366eab1c419fcd880fe265d0041560198612e
Sooram <rkdtnfka@naver.com>
2016-03-12 02:36:30 +0900

    DataStructures assignment#3 StackCalculator

--- CalculatorTest.java
+++ CalculatorTest.java
@@ -4,7 +4,21 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class CalculatorTest
-{
+{	
+	private String string;
+	
+	//constructor
+	public CalculatorTest() 
+	{
+		string = "\0";
+	}
+	
+	public CalculatorTest(String st) 
+	{
+		string = st;
+	}
+	//end constructor
+	
 	public static void main(String args[])
 	{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -29,34 +43,104 @@ public class CalculatorTest
 				System.out.println("입력이 잘못되었습니다. 오류 : " + e.toString());
 			}
 		}
-	}
+	} // end main
 
-	public static final Pattern DIGIT_ERROR_PATTERN = Pattern.compile("[0-9]+\\s+[0-9]+");
-	public static final Pattern OPERATOR_ERROR_PATTERN = Pattern.compile("");
-	public static final Pattern CALCULATE_ERROR_PATTERN = Pattern.compile("");
-	public static final Pattern BRACES_ERROR_PATTERN = Pattern.compile("");
+	
 	
 	private static void command(String input) throws Error
 	{
-		//check whether 'input' is correct or not
-		boolean isErrorInput = false;
-		Matcher m = DIGIT_ERROR_PATTERN.matcher(input);
-    	if(m.find()) {
-    		isErrorInput = m.matches();
-    	}
+		CalculatorTest in = new CalculatorTest(input);
 		
-    	if(isErrorInput) {
-    		throw new Error();
-    	}
+		boolean isCorrectInput = in.isRightInput();
+		if(isCorrectInput == false) {
+			throw new Error();
+		}
+		
+    	boolean isBalBraces = in.isBalanced();
+    	if(isBalBraces == false) {
+			throw new Error();
+		}
     	
-    	//check whether 'input' has balances braces
+    
+    	
+    	
+	} // end command
+	
+	public boolean isRightInput() 
+	{ //check whether 'input' is correct or not
+		boolean isRight = true; 
+		String input = this.string;
+		
+		Pattern digitErrPattern = Pattern.compile("[0-9]+\\s+[0-9]+");
+		Matcher digitErr = digitErrPattern.matcher(input);
+		boolean isDigErr = digitErr.find();
+		if(isDigErr) {
+			isRight = false;
+			return isRight;
+		}
+		
+		input = input.replaceAll(" ","");
+				
+		boolean isBracesErr = input.contains("()");
+		if(isBracesErr) {
+			isRight = false;
+			return isRight;
+		}
+		
+		Pattern opRepPattern = Pattern.compile("[\\+\\*\\^\\/\\%]{2,}");
+		Matcher opRep = opRepPattern.matcher(input);
+		boolean isOpRepErr = opRep.find();
+		if(isOpRepErr) {
+			isRight = false;
+			return isRight;
+		}
+		
+		Pattern opFirstPattern = Pattern.compile("^[\\+\\*\\^\\/\\%]");
+		Matcher opFirst = opFirstPattern.matcher(input);
+		boolean isOpFirstErr = opFirst.find();
+		if(isOpFirstErr) {
+			isRight = false;
+			return isRight;
+		}
+		
+		Pattern opLastPattern = Pattern.compile("[\\+\\*\\^\\/\\%]$");
+		Matcher opLast = opLastPattern.matcher(input);
+		boolean isOpLastErr = opLast.find();
+		if(isOpLastErr) {
+			isRight = false;
+			return isRight;
+		}
+		
+		Pattern divZeroPattern = Pattern.compile("[0-9]+[\\/\\%][0]");
+		Matcher divZero = divZeroPattern.matcher(input);
+		boolean isDivZeroErr = divZero.find();
+		if(isDivZeroErr) {
+			isRight = false;
+			return isRight;
+		}
+		
+		Pattern powerPattern = Pattern.compile("[0][\\^][\\(]?[\\-]");
+		Matcher power = powerPattern.matcher(input);
+		boolean isPowerErr = power.find();
+		if(isPowerErr) {
+			isRight = false;
+			return isRight;
+		}
+			
+		return isRight;
+	}
+
+	public boolean isBalanced() 
+	{ //check whether 'input' has balances braces
+		boolean isBalanced = true;
+		
     	Stack<Character> myStack = new Stack<Character>();
     	
     	boolean balancedSoFar = true;
     	int i = 0;
     	char ch;
-    	while(balancedSoFar && i<input.length()) {
-    		ch = input.charAt(i);
+    	while(balancedSoFar && i<this.string.length()) {
+    		ch = this.string.charAt(i);
     		++i;
     		if(ch == '(') {
     			myStack.push('(');
@@ -73,11 +157,12 @@ public class CalculatorTest
     	
     	if(!balancedSoFar || !myStack.isEmpty()) {
     	//'input' does not have balanced braces
-    		throw new Error();
+    		isBalanced = false;
+    		return isBalanced;
     	} // end if
     	
-    	
-    	
+    	return isBalanced;
 	}
-}
+	
+} // end class
 
